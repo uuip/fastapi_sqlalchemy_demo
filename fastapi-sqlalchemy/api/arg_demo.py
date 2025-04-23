bc18c426eb1abde9c048c6f52f5d2713d9cd17d5
uuip <uuip@users.noreply.github.com>
2025-04-24 00:29:45 +0800

    params example

--- fastapi-sqlalchemy/api/arg_demo.py
+++ fastapi-sqlalchemy/api/arg_demo.py
@@ -1,16 +1,23 @@
 from typing import Annotated, Literal
 
-from fastapi import Query, Path, Body
+from fastapi import Query, Path, Body, APIRouter
 from pydantic import BaseModel, Field
 
-from main import app
+arg_api = APIRouter(prefix="/arg")
 
 
 class Item(BaseModel):
     name: str
     description: str | None = None
+
+
+class Item2(BaseModel):
     price: float
     tax: float | None = None
+
+
+class Item3(BaseModel):
+    address: str
 
 
 class FilterParams(BaseModel):
@@ -20,44 +27,76 @@ class FilterParams(BaseModel):
     tags: list[str] = []
 
 
-@app.get("/arg/{uid}/{item_id}/{item_id2}")
+@arg_api.get("/path/{uid}/{uid2}/{uid3}")
 async def arg_path(
     uid: int,
-    item_id: Annotated[int, Path(title="The ID of the item to get")],
-    item_id2: int = Path(title="The ID of the item to get"),
-) -> int:
-    return uid, item_id, item_id2
+    uid2: Annotated[int, Path(title="The ID of the item to get")],
+    uid3: int = Path(title="The ID of the item to get"),
+):
+    return uid, uid2, uid3
 
 
-@app.get("/arg/")
+@arg_api.get("/query/")
 async def arg_query(
     q: str,
     q2: Annotated[str | None, Query(max_length=50)] = None,
     q3: str | None = Query(default=None, max_length=50),
-) -> int:
+):
     return q, q2, q3
 
 
-@app.get("/arg/")
+# 函数定义中只能有一次模型类Query定义
+@arg_api.get("/query_model/")
 async def arg_query_model(
-    filter_query: Annotated[FilterParams, Query()],
-    filter_query2: FilterParams = Query(),
+    # filter_query: Annotated[FilterParams, Query()],
+    filter_query: FilterParams = Query(),
 ):
-    return filter_query, filter_query2
+    return filter_query
 
 
-@app.post("/arg/")
+# {
+#     "name": "string",
+#     "description": "string",
+# }
+@arg_api.post("/body/")
 async def arg_body(
     item: Item,
-    item2: Annotated[Item, Body()],
-    item3: Item = Body(),
-) -> int:
+):
+    return item
+
+
+# {
+#   "item": {
+#     "name": "string",
+#     "description": "string"
+#   },
+#   "item2": {
+#     "price": 0,
+#     "tax": 0
+#   },
+#   "item3": {
+#     "address": "string"
+#   }
+# }
+@arg_api.post("/body_multi/")
+async def arg_body_multi(
+    item: Item,
+    item2: Annotated[Item2, Body()],
+    item3: Item3 = Body(),
+):
     return item, item2, item3
 
 
-@app.post("/arg/")
+@arg_api.post("/body_2single/")
 async def arg_body_single(
     importance: Annotated[int, Body()],
-    importance2: int = Body(),  # embed=True
-) -> int:
+    importance2: int = Body(),
+):
     return importance, importance2
+
+
+@arg_api.post("/body_1single/")
+async def arg_body_single(
+    importance: Annotated[int, Body(embed=True)],
+):
+    return importance
